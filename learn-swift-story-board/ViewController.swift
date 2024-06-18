2b7cd2cabfe655bf437ab40353aa18ac1535d588
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-18 19:14:19 +0700

    Initial Commit

---
+++ learn-swift-story-board/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learn-swift-story-board
+//
+//  Created by Nguyen Quang Minh on 18/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
